4086c004a6a93154946a2409605e54d22db71454
bogeunsss <bogun11904@gmail.com>
2021-07-08 19:36:55 +0900

    FEAT: Create Reply model

---
+++ backend/src/main/java/com/villain/backend/model/Reply.java
@@ -0,0 +1,36 @@
+package com.villain.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Reply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "boardId")
+    private Board board;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+}
