485ccc71e808a810b3fbf1b1914ebabf82b9a1f9
bogeunsss <bogun11904@gmail.com>
2021-07-08 17:58:30 +0900

    FEAT: Create User model

---
+++ backend/src/main/java/com/villain/backend/model/User.java
@@ -0,0 +1,30 @@
+package com.villain.backend.model;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 20)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @ColumnDefault("'user'")
+    private String role;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+}
